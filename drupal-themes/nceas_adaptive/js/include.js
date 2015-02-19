31838121702d385e9358a232e36b7ea37bbfe084
Lauren <walker@nceas.ucsb.edu>
2015-02-19 15:27:50 -0800

    Remove old header background images. Update site favicon.

--- drupal-themes/nceas_adaptive/js/include.js
+++ drupal-themes/nceas_adaptive/js/include.js
@@ -11,6 +11,14 @@ jQuery(window).load(function(){
 	
 	//Make sure the flexslider is loaded.... there is a bug in the Flexslider module
 	var $ = jQuery;
-	$(".flexslider").flexslider();
+	var slideshow = $(".flexslider");
+	if(slideshow.length > 0) $(slideshow).flexslider();
+	
+	//Set up the listener for the Portal dropdown menu which will take the user to the link specified
+	$("#portal-go").click(function(){
+		//Find the select input from the form
+		var link = $("#portal-go-form").val();
+		window.location = link;
+	});
 	
 });
